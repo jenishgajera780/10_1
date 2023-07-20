22f09e75cd6d38426a745dadacd89af57c0c2f4e
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-20 18:09:34 +0530

    Add files via upload

---
+++ 10_1_Q1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+cube()
+
+{
+	int o,j;
+	
+	printf("write any number = ");
+	scanf("%d",&a);
+	j = o * o * o ;
+	
+	printf("Cube of given number = %d",b);
+
+}
+
+void main()
+
+{
+
+	cube();
+
+}
---
+++ 10_1_Q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+prop(){
+	
+    
+    int j,o;
+	
+	printf("type Value to find number is devisible by 3 & 5 = ");
+	scanf("%d",&j);
+	
+	if(j % 3 == 0 && j % 5 == 0){
+    printf("The given number %d is devisiable by 3 & 5 ");
+	}else{
+
+printf("given number %d is not devisiable by 3 & 5 ");}}
+
+
+void main(){
+
+	prop();
+
+}
